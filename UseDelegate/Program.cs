17bd7e5d90dba21780a502acd46cec63dc407b47
pphilio <pphilio@naver.com>
2019-04-02 14:32:34 +0900

    using delegate

--- UseDelegate/Program.cs
+++ UseDelegate/Program.cs
@@ -8,8 +8,23 @@ namespace UseDelegate
 {
     class Program
     {
+        delegate int Mydelegate(int a, int b);
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
         static void Main(string[] args)
         {
+
+            Mydelegate callback;
+            callback = new Mydelegate(Plus);
+            Console.WriteLine(callback(3, 5));
+            callback = new Mydelegate(Minus);
+            Console.WriteLine(callback(7, 3));
         }
     }
 }
